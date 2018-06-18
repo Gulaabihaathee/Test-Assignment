e46790572243ed5a89d5cd691b58896aa51bc05e
Filip <32573906+Gulaabihaathee@users.noreply.github.com>
2018-06-18 21:00:36 +0200

    visual correction

--- test_assignment/groups/serializers.py
+++ test_assignment/groups/serializers.py
@@ -1,10 +1,9 @@
 from rest_framework import serializers
 from .heromodel import Hero
 
+
 class HeroSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Hero
         fields = '__all__'
-
-
