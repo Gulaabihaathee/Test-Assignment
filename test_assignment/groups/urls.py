513e491b73e9bf7fe50b5699e56c303215fdea97
Filip <32573906+Gulaabihaathee@users.noreply.github.com>
2018-06-18 21:04:27 +0200

    visual correction

--- test_assignment/groups/urls.py
+++ test_assignment/groups/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from . import views
 from rest_framework.urlpatterns import format_suffix_patterns
+from . import views
 
 urlpatterns = [
     path('', views.ranking, name='ranking'),
