8a9a8846a95c63ae637c7ecf049689cf53b432d2
Filip <32573906+Gulaabihaathee@users.noreply.github.com>
2018-06-18 20:53:10 +0200

    Order correction, comment deleted

--- test_assignment/groups/heromodel.py
+++ test_assignment/groups/heromodel.py
@@ -1,9 +1,9 @@
 from django.db import models
-from .groupmodel import Group
 from datetime import datetime
+from .groupmodel import Group
+
 
 class Hero(models.Model):
-    #id = models.PositiveIntegerField(primary_key=True)
     group = models.ForeignKey(Group, on_delete=models.CASCADE, blank=True, null=True)
     name = models.CharField(max_length=250, blank=True)
     wins_counter = models.PositiveIntegerField(default=0)
@@ -11,7 +11,8 @@ class Hero(models.Model):
     is_killed = models.BooleanField(default=False)
     murder_date = models.DateTimeField(null=True, blank=True)
     defeat_date = models.DateTimeField(null=True, blank=True)
-
+    
+    
     def __str__(self):
         return self.name
 
