fd79f55d4d1368b736593c9c4bb443696942d4ca
Filip <32573906+Gulaabihaathee@users.noreply.github.com>
2018-06-18 21:07:38 +0200

    uneccessary comments erased

--- test_assignment/test_assignment/settings.py
+++ test_assignment/test_assignment/settings.py
@@ -3,14 +3,9 @@ from .settings_secret import *
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
-
-# Application definition
 
 INSTALLED_APPS = [
     'django.contrib.admin',
